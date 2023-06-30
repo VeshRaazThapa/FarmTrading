d1d420d7fa6a3579aacf1ab8fa628e2814ff21b8
himalg7 <thapahimal777@gmail.com>
2023-06-30 11:49:02 +0545

    map distance calculation using haversine formula

--- frontend/flutter/agro_millets/lib/core/map/presentation/map_page.dart
+++ frontend/flutter/agro_millets/lib/core/map/presentation/map_page.dart
@@ -10,8 +10,11 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
-
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
+import 'dart:math';
 import '../../../models/user.dart';
+import 'package:agro_millets/data/cache/app_cache.dart';
+import 'package:url_launcher/url_launcher.dart' as UrlLauncher;
 
 class MapPage extends ConsumerStatefulWidget {
   const MapPage({super.key});
@@ -24,6 +27,7 @@ class _MapPageState extends ConsumerState<MapPage> {
   late MapManager mapManager;
   MapController mapController = MapController();
   List<LatLng> farmerCoordinates = [];
+  var logged_in_user = appState.value.user;
 
   @override
   void initState() {
@@ -41,7 +45,6 @@ class _MapPageState extends ConsumerState<MapPage> {
   Widget build(BuildContext context) {
     List<Marker> buildMarkers() {
       List<User> map = ref.watch(mapProvider).getMap();
-
       if (map.isEmpty) {
         return [Marker(point:LatLng(27.694549317783395, 85.32055500746131),builder: (BuildContext context) {
           return Icon(Icons.location_on, color: Colors.red);
@@ -49,15 +52,36 @@ class _MapPageState extends ConsumerState<MapPage> {
       }
 
       return map.map((User user) {
-          return Marker(
+        Coordinate coord1 = Coordinate(user.latitude,user.longitude); // Berlin coordinates
+        Coordinate coord2 = Coordinate(logged_in_user?.latitude , logged_in_user?.longitude); // Paris coordinates
+
+        double distance = distanceBetweenCoordinates(coord1, coord2);
+
+        return Marker(
             point: LatLng(user.latitude,user.longitude),
             builder: (ctx) => GestureDetector(
+
               onTap: () {
+
                 showDialog(
                   context: context,
                   builder: (BuildContext context) => AlertDialog(
                     title: Text(user.name),
-                    content: Text(user.userType + "\n" + user.phone,style: const TextStyle(fontSize: 16),),
+                    content: Row(
+                      children: [
+                        Expanded(
+                          child: Text(
+                            user.userType + "\n" + user.phone + "\n" + "Distance: " + distance.toStringAsFixed(2) + " KM",
+                            style: const TextStyle(fontSize: 16),
+                          ),
+                        ),
+                        IconButton(
+                          onPressed: () => UrlLauncher.launch('tel://${user.phone}'),
+                          icon: const Icon(MdiIcons.phoneDial),
+                        ),
+                      ],
+                    ),
+
                     actions: <Widget>[
                       TextButton(
                         child: Text('Close'),
@@ -69,7 +93,25 @@ class _MapPageState extends ConsumerState<MapPage> {
                   ),
                 );
               },
-              child: Icon(Icons.location_on, color: Colors.red),
+              child: Row(
+                children: [
+                  Icon(Icons.location_on, color: Colors.red),
+                  // SizedBox(width: 8),
+                  //
+                  Container(
+                    color: Colors.white, // Background color
+                    child: Text(
+                      distance.toStringAsFixed(2) + " KM",
+                      style: TextStyle(
+                        fontSize: 10,
+                        fontWeight: FontWeight.normal,
+                        color: Colors.blue, // Font color
+                      ),
+                    ),
+                  ),
+
+                ],
+              ),
             ),
             // builder: (BuildContext context) {
             //   return Icon(Icons.location_on, color: Colors.red);
@@ -81,7 +123,7 @@ class _MapPageState extends ConsumerState<MapPage> {
 
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Your Map"),
+        title: const Text("Sellers Near You"),
       ),
       body: SizedBox(
         // height: 200, // Set the desired height for the map
@@ -92,7 +134,7 @@ class _MapPageState extends ConsumerState<MapPage> {
               mapController: mapController,
 
               options: MapOptions(
-                center: LatLng(27.794549317783395, 85.62055500746131),
+                center: LatLng(logged_in_user!.latitude, logged_in_user!.longitude),
                 zoom: 9,
                 minZoom: 2, // Set the minimum zoom level
                 maxZoom: 18,
@@ -144,4 +186,35 @@ class _MapPageState extends ConsumerState<MapPage> {
 
     );
   }
+}
+
+
+class Coordinate {
+  final double? latitude;
+  final double? longitude;
+
+  Coordinate(this.latitude, this.longitude);
+}
+
+double distanceBetweenCoordinates(Coordinate coord1, Coordinate coord2) {
+  const double earthRadius = 6371; // Radius of the Earth in kilometers
+
+  double degToRad(double? degree) {
+    return degree! * (pi / 180);
+  }
+
+  double lat1 = degToRad(coord1.latitude);
+  double lon1 = degToRad(coord1.longitude);
+  double lat2 = degToRad(coord2.latitude);
+  double lon2 = degToRad(coord2.longitude);
+
+  double dlat = lat2 - lat1;
+  double dlon = lon2 - lon1;
+
+  double a = pow(sin(dlat / 2), 2) +
+      cos(lat1) * cos(lat2) * pow(sin(dlon / 2), 2);
+  double c = 2 * atan2(sqrt(a), sqrt(1 - a));
+
+  double distance = earthRadius * c;
+  return distance;
 }
