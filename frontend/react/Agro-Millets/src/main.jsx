8a09fae5120622d03a392fa76c46a862b4492069
himalg7 <thapahimal777@gmail.com>
2023-07-22 11:48:32 +0545

    files directories managed

--- frontend/react/Agro-Millets/src/main.jsx
+++ frontend/react/Agro-Millets/src/main.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 import {Route, HashRouter, Switch} from "react-router-dom";
-import About from "./pages/About/presentation/About";
+// import About from "./pages/About/presentation/About";
 import "@fortawesome/fontawesome-free/css/all.css";
 
 import "react-toastify/dist/ReactToastify.css";
-import Search from "./pages/Search/presentation/Search";
-import AdminLayout from "./layouts/admin";
-import ShopLayout from "./layouts/shop";
-import LoginLayout from "./layouts/login";
-import ProfileLayout from "./layouts/profile";
-import Login from "./pages/Login/presentation/Login.jsx";
+import Search from "./views/Search/presentation/Search";
+import AdminLayout from "./navigations/admin";
+import ShopLayout from "./navigations/shop";
+import LoginLayout from "./navigations/login";
+import ProfileLayout from "./navigations/profile";
+import Login from "./views/Login/presentation/Login.jsx";
 import "./../src/assets/css/App.css";
 import "./index.css";
 import {ChakraProvider} from "@chakra-ui/react";
@@ -27,7 +27,6 @@ ReactDOM.render(
                         <Route path="/shop" component={ShopLayout}/>
                         <Route path="/login" component={LoginLayout}/>
                         <Route path="/profile" component={ProfileLayout}/>
-                        <Route path="/about" component={About}/>
                         <Route path="/search" component={Search}/>
                         <Route path="/admin" component={AdminLayout}/>
                     </Switch>
